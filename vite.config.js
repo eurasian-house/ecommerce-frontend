fb70dcf4b03455db22d5ccae5f1f2ab33b5a3ba2
Mohd Saquib <contacteurasianhouse@gmail.com>
2026-06-23 14:16:56 +0530

    vercelJson file modify

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { visualizer } from "rollup-plugin-visualizer";
 
 export default defineConfig({
   plugins: [react()],
@@ -13,13 +12,4 @@ export default defineConfig({
       },
     },
   },
-  plugins: [
-    react(),
-    visualizer({
-      open: true,
-      gzipSize: true,
-      brotliSize: true,
-      filename: "bundle-analysis.html",
-    }),
-  ],
 })
